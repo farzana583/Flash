8ec3af3be0e6dcc8426150cf6a77d91b727762fe
farzana583 <farzanaisack@gmail.com>
2021-01-29 00:32:57 +0300

    add  flashapp/

---
+++ flashapp/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_photo = models.ImageField(upload_to="images/")
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+class Deck(models.Model):
+    title = models.TextField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Card(models.Model):
+    title = models.TextField(max_length=150)
+    notes = models.TextField(max_length=300)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
---
+++ flashapp/views.py
@@ -0,0 +1,92 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.core.exceptions import *
+from .forms import SignUpForm, AddDeckForm, AddCardForm
+from .models import Deck, Card
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url=('signup/'))
+def homepage(request):
+    decks = Deck.objects.filter(user_id=request.user.id)
+    return render(request, 'homepage.html', {"decks": decks})
+
+def deck(request, deck):
+    current_deck = Deck.objects.get(id=deck)
+    cards = Card.objects.filter(deck_id=deck)
+    return render(request, 'deck.html', {"deck": current_deck, "cards": cards})
+
+def add_deck(request):
+    if request.method == 'POST':
+        form = AddDeckForm(request.POST)
+        if form.is_valid():
+            deck = Deck(title=form.cleaned_data.get('title'),
+                        user=request.user)
+            deck.save()
+            return redirect('homepage')
+        else:
+            return render(request, 'add-deck.html', {'form': form})
+    else:
+        form = AddDeckForm()
+        return render(request, 'add-deck.html', {'form': form})
+
+def add_card(request, deck):
+    if request.method == 'POST':
+        form = AddCardForm(request.POST)
+        if form.is_valid():
+            card = Card(title=form.cleaned_data.get('title'),
+                        notes=form.cleaned_data.get('notes'),
+                        deck=Deck.objects.get(id=deck))
+            card.save()
+            return redirect('homepage')
+        else:
+            return render(request, 'add-card.html', {'form': form})
+    else:
+        form = AddCardForm()
+        return render(request, 'add-card.html', {'form': form})
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("handling signup")
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.profile.profile_photo = form.cleaned_data.get('profile_photo')
+            user.profile.bio = form.cleaned_data.get('bio')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            print("signed up")
+            return redirect('homepage')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
+
+
+def delete_view(request,card):
+    current_card=Card.objects.get(id=card)
+    deck=current_card.deck
+    current_card.delete()
+    return render(request,'deck.html',{'deck':deck})
+    
+
+def update_view(request,id):
+    context={}       
+    obj= get_object_or_404(Card, id=id)
+
+    if request.method =="POST":
+        obj.delete()
+        return HttpResponseRedirect('homepage')
+    return render (request, 'update_views.html')
